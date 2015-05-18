c26fa890f3c95319b2463d929d1c38986342e1b0
Oliver Thomas <olliethomas86@gmail.com>
2015-05-18 02:04:32 +0100

    Fixed a typo causing post links in nav bar to fail

---
+++ js/header.js
@@ -0,0 +1,33 @@
+var colours = ["#2E8AE6", "#FF5050", "#70DB70", "#FFA347", "#FF99CC", "#CC99FF"];
+
+function circleColor() {
+    /**Index**/
+    if (window.location.href.indexOf("index") > -1) {
+        document.getElementById("circle").style.fill = colours[0];
+    } else {}
+
+    /**Problem Sets**/
+    if (window.location.href.indexOf("blog") > -1) {
+        document.getElementById("circle").style.fill = colours[1];
+    }
+
+    /**Projects**/
+    if (window.location.href.indexOf("joinus") > -1) {
+        document.getElementById("circle").style.fill = colours[2];
+    }
+
+    /**About**/
+    if (window.location.href.indexOf("members") > -1) {
+        document.getElementById("circle").style.fill = colours[3];
+    }
+
+    /**Contact**/
+    if (window.location.href.indexOf("problems") > -1) {
+        document.getElementById("circle").style.fill = colours[4];
+    }
+
+    /**Forum**/
+    if (window.location.href.indexOf("projects") > -1) {
+        document.getElementById("circle").style.fill = colours[5];
+    }
+}
