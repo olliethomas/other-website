36b553502228936566631168abb12f6641bbc9f2
Oliver Thomas <olliethomas86@gmail.com>
2015-05-20 18:29:07 +0100

    played with the js to change the color of header border to match logo color

--- js/header.js
+++ js/header.js
@@ -4,30 +4,37 @@ function circleColor() {
     /**Index**/
     if (window.location.href.indexOf("index") > -1) {
         document.getElementById("circle").style.fill = colours[0];
+        document.getElementById("header-wrapper").style.borderColor = colours[0];
     } else {}
 
     /**Problem Sets**/
     if (window.location.href.indexOf("blog") > -1) {
         document.getElementById("circle").style.fill = colours[1];
+        document.getElementById("header-wrapper").style.borderColor = colours[1];
     }
-
+    
     /**Projects**/
     if (window.location.href.indexOf("joinus") > -1) {
         document.getElementById("circle").style.fill = colours[2];
+        document.getElementById("header-wrapper").style.borderColor = colours[2];
+
     }
 
     /**About**/
     if (window.location.href.indexOf("members") > -1) {
         document.getElementById("circle").style.fill = colours[3];
+        document.getElementById("header-wrapper").style.borderColor = colours[3];
     }
 
     /**Contact**/
     if (window.location.href.indexOf("problems") > -1) {
         document.getElementById("circle").style.fill = colours[4];
+        document.getElementById("header-wrapper").style.borderColor = colours[4];
     }
 
     /**Forum**/
     if (window.location.href.indexOf("projects") > -1) {
         document.getElementById("circle").style.fill = colours[5];
+        document.getElementById("header-wrapper").style.borderColor = colours[0];
     }
 }
